a103cadc6a44b9f1c59427269029d2864b7a6583
hanyx13 <hanyx13@foxmail.com>
2019-09-24 09:32:26 +0800

    Initial commit

---
+++ src/main/java/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+/**
+ * springCloud的服务注册中心
+ *
+ * @author hanyuxuan
+ * @data 2019/9/20
+ * @time 13:52
+ */
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
